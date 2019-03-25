2a339ea372990b332217cb709e91e3cbb09b3fdf
Juan Grenz <j.grenz@hotmail.com>
2019-03-24 21:55:53 -0300

    Se agrega metodo getZodiaco en clase zodiaco

--- src/main/java/dto/Zodiaco.java
+++ src/main/java/dto/Zodiaco.java
@@ -1,61 +1,97 @@
 package dto;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+public class Zodiaco
+{
 
-public enum Zodiaco { 
+	public String getZodiaco(int dia, int mes) {
 	 
-    ARIES(newDate("03" ,"21"), newDate("04","20")), 
-    TAURUS(newDate("04","21"), newDate("05","21")), 
-    GEMINI(newDate("05","22"), newDate("06","21")), 
-    CANCER(newDate("06","22"), newDate("07","22")), 
-    LEO(newDate("07","23"), newDate("08","22")), 
-    VIRGO(newDate("08","23"), newDate("09","23")), 
-    LIBRA(newDate("09","24"), newDate("10","23")), 
-    SCORPIO(newDate("10","24"), newDate("11","22")), 
-    SAGITTARIUS(newDate("11","23"), newDate("12","21")), 
-    CAPRICORN(newDate("12","22"), newDate("01","20")), 
-    AQUARIUS(newDate("01","21"), newDate("02","19")), 
-    PISCES(newDate("02","20"), newDate("03","20")); 
- 
-    private Date from; 
-    private Date to; 
-     
-    private Zodiaco(Date from, Date to) { 
-        this.from = from; 
-        this.to = to; 
-    } 
- 
-    private static Date newDate(String mes, String dia) {
-    	Date date = null;
-		try {
-			SimpleDateFormat originalFormat = new SimpleDateFormat("MMdd");
-			return date = originalFormat.parse(mes.concat(dia));
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-    	return date; 
-    } 
-     
-    public static Zodiaco toZodiac(Date cumpleaños) { 
-		
-        for (Zodiaco zodiaco: values()) { 
-           Date inicio = zodiaco.from;
-           Date fin = zodiaco.to; 
-           
- 
-            if (cumpleaños.after(inicio)) { 
-            	if(cumpleaños.before(fin)) {
-            		return zodiaco;
-            	}
-            }
- 
-            if (cumpleaños.after(fin)) { 
-            	continue; 
-            } 
-        } 
-         
-        throw new IllegalArgumentException("Cannot find zodiac sign for date: " + cumpleaños); 
-    } 
+		String signo = "";
+	    switch (mes) {
+	        case 0:
+	            if (dia > 21) {
+	                signo = "ACUARIO";
+	            } else {
+	                signo = "CAPRICORNIO";
+	            }
+	            break;
+	        case 1:
+	                if (dia > 19) {
+	                    signo = "PISCIS";
+	                } else {
+	                    signo = "ACUARIO";
+	                }
+	                break;
+	        case 2:
+	                if (dia > 20) {
+	                    signo = "ARIES";
+	                } else {
+	                    signo = "PISCIS";
+	                }
+	                break;
+	        case 3:
+	                if (dia > 20) {
+	                    signo = "TAURO";
+	                } else {
+	                    signo = "ARIES";
+	                }
+	                break;
+	        case 4:
+	                if (dia > 21) {
+	                    signo = "GEMINIS";
+	                } else {
+	                    signo = "TAURO";
+	                }
+	                break;
+	        case 5:
+	                if (dia > 20) {
+	                    signo = "CANCER";
+	                } else {
+	                    signo = "GEMINIS";
+	                }
+	                break;
+	        case 6:
+	                if (dia > 22) {
+	                    signo = "LEO";
+	                } else {
+	                    signo = "CANCER";
+	                }
+	                break;
+	        case 7:
+	                if (dia > 21) {
+	                    signo = "VIRGO";
+	                } else {
+	                    signo = "LEO";
+	                }
+	                break;
+	        case 8:
+	                if (dia > 22) {
+	                    signo = "LIBRA";
+	                } else {
+	                    signo = "VIRGO";
+	                }
+	                break;
+	        case 9:
+	                if (dia > 22) {
+	                    signo = "ESCORPION";
+	                } else {
+	                    signo = "LIBRA";
+	                }
+	                break;
+	        case 10:
+	                if (dia > 21) {
+	                    signo = "SAGITARIO";
+	                } else {
+	                    signo = "ESCORPION";
+	                }
+	                break;
+	        case 11:
+	                if (dia > 21) {
+	                    signo = "CAPRICORNIO";
+	                } else {
+	                    signo = "SAGITARIO";
+	                }
+	        break;
+	    }
+	    return signo;
+	}
 }
